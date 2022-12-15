e666f5bfd5f86ccb013344d23c827979ce6a614d
Arman Zhankin <azhankin@macos-c02cg1cwmd6n.attlocal.net>
2022-12-14 23:49:13 -0800

    Change DB engine

--- db/base.py
+++ db/base.py
@@ -1,29 +1,13 @@
-# from flask_sqlalchemy import SQLAlchemy
-#
-# db = SQLAlchemy()
-
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-#
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/fb'
-#
-# db = SQLAlchemy(app)
-#
-#
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#
-#     def __repr__(self):
-#         return '<User %r>' % self.username
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql+pymysql://root:root@localhost:3306/fb')
+url = url = "mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(
+    os.environ['DB_USER'], os.environ['DB_PASSWORD'], os.environ['DB_HOST'], os.environ['DB_PORT'],
+    os.environ['DB']
+)
+engine = create_engine(url)
 # create a configured "Session" class
 Session = sessionmaker(bind=engine)
 
